b948a0b88f051aad45e1695b2e2be222233b5d91
Guilherme-Valle <guidovalle1608@gmail.com>
2022-03-18 20:52:56 -0300

    Inicia projeto React com webpack e babel

---
+++ frontend/src/components/LoremIpsum.jsx
@@ -0,0 +1,6 @@
+export function LoremIpsum() {
+    return <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Phasellus eget facilisis urna.
+        Aliquam vestibulum tempor libero eget egestas.
+        Etiam vitae nisi ut lacus pellentesque suscipit</h1>
+}
