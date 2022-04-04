a748d61d9e446e13a24b79be5f52a1c02d82c5ab
mateuscsilva <mateuscsilva.1@gmail.com>
2022-04-04 12:08:45 -0300

    acrescenta migrations e primeira versao de models de area e subarea

---
+++ backend/app/Models/Area.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Enum;
+use Laravel\Fortify\Rules\Password;
+use Laravel\Sanctum\HasApiTokens;
+
+class Area extends BaseModel
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'area_name',
+        'program_id',
+    ];
+
+    public function belongsToTheProgram()
+    {
+        return $this->hasOne(Program::class, 'program_id');
+    }
+
+    public static function creationRules(): array
+    {
+        return [
+            'area_name' => 'required|string|max:255',
+            'program_id' => [
+                'nullable',
+                'int',
+                Rule::exists(Program::class, 'id'),
+                'required',
+            ],
+        ];
+    }
+
+    public static function updateRules(): array
+    {
+        return [
+            'area_name' => 'required|string|max:255',
+            'program_id' => [
+                'nullable',
+                'int',
+                Rule::exists(Program::class, 'id'),
+                'required',
+            ],
+        ];
+    }
+
+    public static function createOrUpdateArea(array $data): Area
+    { 
+        return User::updateOrCreateModel(
+            Arr::only($data, ['area_name']),
+            $data
+        );
+    }
+}
