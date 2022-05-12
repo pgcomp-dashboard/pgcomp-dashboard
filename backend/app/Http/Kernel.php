4f5801a643e3b3b45eb6f23a7c2c459c5b7b348c
Litiano Moura <litiano.moura@yahoo.com.br>
2022-05-12 09:07:43 -0300

    Backend: Disbled middleware csrf.

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -61,7 +61,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
-//        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class, //@todo desabilitado para teste de carga!
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
 }
