b428a9b6b223463f85a2a6d4de3ac374575a58d7
Litiano Moura <litiano.moura@yahoo.com.br>
2022-05-23 19:03:38 -0300

    Admin: fix axios config.

--- frontend-admin/src/services/api.ts
+++ frontend-admin/src/services/api.ts
@@ -1,3 +1,3 @@
 import axios, { AxiosInstance } from "axios";
 
-export const api: AxiosInstance = axios.create({baseURL: "https://mate85-api.litiano.dev.br/api/portal/admin", withCredentials: true});
+export const api: AxiosInstance = axios.create({ baseURL: "https://mate85-api.litiano.dev.br/api/portal/admin" });
