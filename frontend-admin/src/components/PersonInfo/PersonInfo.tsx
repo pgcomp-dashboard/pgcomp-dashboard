1b87b71e193d6f52c4f781df14df0db161db6a77
Guilherme-Valle <guidovalle1608@gmail.com>
2022-05-30 00:09:11 -0300

    Add data fetching of user info

--- frontend-admin/src/components/PersonInfo/PersonInfo.tsx
+++ frontend-admin/src/components/PersonInfo/PersonInfo.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useMatch } from "react-router-dom";
+import { api } from "../../services/api";
 
 function PersonInfo(props: any) {
     const match = useMatch(":sessionType/:id");
     const sessionType = match?.params.sessionType;
     const id = match?.params.id;
+    const [personData, setPersonData] = useState({});
+
+    useEffect(() => {
+        async function fetchData() {
+            const { data } = await api.get(`${sessionType}/${id}`);
+            setPersonData(data);
+        };
+        fetchData();
+    }, []);
+
     return <>
-        <h1>Session type: {sessionType} Id: {id}</h1>
+        <p><small>{JSON.stringify(personData)}</small></p>
     </>;
 }
 
