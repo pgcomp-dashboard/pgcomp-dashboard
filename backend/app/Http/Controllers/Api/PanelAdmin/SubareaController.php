895ea32822f3866888778460a6ab5e0c4b0388f8
mateuscsilva <mateuscsilva.1@gmail.com>
2022-06-10 15:14:13 -0300

    add checking step - store new area or subarea

--- backend/app/Http/Controllers/Api/PanelAdmin/SubareaController.php
+++ backend/app/Http/Controllers/Api/PanelAdmin/SubareaController.php
@@ -6,12 +6,26 @@ use App\Http\Controllers\Api\BaseApiResourceController;
 use App\Models\Area;
 use App\Models\BaseModel;
 use App\Models\Subarea;
+use Illuminate\Http\Request;
 
 class SubareaController extends BaseApiResourceController{
 
     protected function modelClass(): string|BaseModel
     {
         return Subarea::class;
+    }
+
+    public function store(Request $request)
+    {
+        $msg = "Essa subárea já existe, deseja criar outra com o mesmo nome?";
+
+        $subarea = Subarea::where('subarea_name', $request->input('subarea_name'))->get();
+        $confirm = $request->input('confirm');
+        if(count($subarea) == 0 || $confirm){
+            return parent::store($request);
+        }else{
+            return $msg;
+        }
     }
 
     public function destroy(int $id)
