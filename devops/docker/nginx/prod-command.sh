831766fdc8b9814248032cc6d0900630720ffa29
Felipe Paixão <fleap@fleap.dev>
2025-04-09 18:25:47 -0300

    chore: remove old docker/devops related files

--- devops/docker/nginx/prod-command.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-while :; do
-  sleep 6h &
-  wait ${!}
-  test -f /etc/letsencrypt/restart-nginx && rm -f /etc/letsencrypt/restart-nginx && nginx -s reload
-done &
-
-nginx -g "daemon off;"
