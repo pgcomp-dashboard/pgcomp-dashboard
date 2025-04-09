adc87256acc63be936bfd48529660d5ae119f7f3
Felipe Paixão <fleap@fleap.dev>
2025-04-09 18:25:47 -0300

    chore: remove old docker/devops related files

--- devops/docker/certbot/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-trap exit TERM
-while :; do
-  certbot renew --deploy-hook "touch /etc/letsencrypt/restart-nginx"
-  sleep 12h &
-  wait ${!}
-done
--- devops/docker/install-portainer.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker volume create portainer_data
-
-docker run -d -p 9443:9443 --name portainer \
-    --restart=always \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v portainer_data:/data \
-    portainer/portainer-ce
