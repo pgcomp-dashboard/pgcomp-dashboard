e23d329947583387aaed0672598ee50d5c884f26
Felipe Paixão <fleap@fleap.dev>
2025-04-20 12:48:52 -0300

    feat(frontend): setup eslint rules

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { BrowserRouter, Routes, Route, } from 'react-router';
 import NotFoundPage from '@/pages/not-found';
-import './App.css'
+import './App.css';
 
 function App() {
   return (
-      <BrowserRouter>
-          <Routes>
-              <Route index element={<>Hello</>} />
-              <Route path={"*"} element={<NotFoundPage />}/>
-          </Routes>
-      </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<>Hello</>} />
+        <Route path={'*'} element={<NotFoundPage />}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
