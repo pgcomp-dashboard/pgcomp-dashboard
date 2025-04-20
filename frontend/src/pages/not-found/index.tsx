30f30f43542c06c58bdb9de592ab66f74ea29caa
Felipe Paixão <fleap@fleap.dev>
2025-04-20 12:48:52 -0300

    feat(frontend): setup eslint rules

--- frontend/src/pages/not-found/index.tsx
+++ frontend/src/pages/not-found/index.tsx
@@ -1,14 +1,14 @@
-import { NavLink }  from 'react-router';
+import { NavLink, }  from 'react-router';
 
 export default function Index() {
-    return (
-        <div className="w-full h-full min-h-screen flex flex-col items-center justify-around">
-            <h1 className="text-4xl font-bold p-2">Página não encontrada</h1>
-            <div>
-                <p className="pb-5 text-black/75 dark:text-white/75">Essa página não foi encontrada na nossa aplicação.</p>
-                <NavLink to="/" className="p-2 underline">Voltar para o início</NavLink>
-            </div>
-            <div></div>
-        </div>
-    )
+  return (
+    <div className="w-full h-full min-h-screen flex flex-col items-center justify-around">
+      <h1 className="text-4xl font-bold p-2">Página não encontrada</h1>
+      <div>
+        <p className="pb-5 text-black/75 dark:text-white/75">Essa página não foi encontrada na nossa aplicação.</p>
+        <NavLink to="/" className="p-2 underline">Voltar para o início</NavLink>
+      </div>
+      <div></div>
+    </div>
+  );
 }
