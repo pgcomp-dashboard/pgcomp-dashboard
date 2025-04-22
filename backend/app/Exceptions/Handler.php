34ced35c9af949b04c9ee7a3015e373b444eb442
Felipe Paixão <fleap@fleap.dev>
2025-04-22 15:59:38 -0300

    feat(backend): define a standard format for Api errors

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -2,7 +2,11 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Log;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -13,7 +17,9 @@ class Handler extends ExceptionHandler
      * @var array<int, class-string<Throwable>>
      */
     protected $dontReport = [
-        //
+        ValidationException::class,
+        AuthorizationException::class,
+        AuthenticationException::class,
     ];
 
     /**
@@ -35,7 +41,46 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
+            if ($this->shouldReport($e)) {
+                Log::error(
+                    'Unhandled Exception: '.$e->getMessage(),
+                    ['exception' => $e]
+                );
+            }
+        });
+
+        $this->renderable(function (ValidationException $e) {
+            return response()->json(
+                ['errors' => array_map(fn($e) => ['description' => $e], $e->validator->errors()->all())],
+                400,
+            );
+        });
+
+        $this->renderable(function (AuthenticationException $e) {
+            return response()->json(
+                ['errors' => [
+                    ['description' => 'Não autenticado']
+                ]],
+                401,
+            );
+        });
+
+        $this->renderable(function (AuthorizationException $e) {
+            return response()->json(
+                ['errors' => [
+                    ['description' => 'Não autorizado']
+                ]],
+                403,
+            );
+        });
+
+        $this->renderable(function (Throwable $e) {
+            return response()->json(
+                ['errors' => [
+                    ['description' => $e->getMessage()],
+                ]],
+                500,
+            );
         });
     }
 }
