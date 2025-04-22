c7a03e211b1343d1d65fd267677bc2f2aae755f1
Felipe Paixão <fleap@fleap.dev>
2025-04-22 19:39:55 -0300

    feat(backend): create the "Guest" user type to enable system access to non-professors/students

--- backend/app/Enums/UserType.php
+++ backend/app/Enums/UserType.php
@@ -10,12 +10,14 @@ enum UserType: string
 
     case STUDENT = 'student';
     case PROFESSOR = 'professor';
+    case GUEST = 'guest';
 
     public function label(): string
     {
         return match ($this) {
             self::PROFESSOR => 'doscente',
             self::STUDENT => 'discente',
+            self::GUEST => 'visitante',
         };
     }
 }
