a5a98102ebcf39d8b3daf365f3c182da98d6fc58
julianansantos <julianansantos22@gmail.com>
2025-04-24 09:32:30 -0300

    feat(backend): returning the delete function to the original code to refactor it again later

--- backend/app/Http/Controllers/Api/PanelAdmin/AreaController.php
+++ backend/app/Http/Controllers/Api/PanelAdmin/AreaController.php
@@ -34,17 +34,20 @@ class AreaController extends BaseApiResourceController
 
     public function destroy(int $id)
     {
-        $area = Area::find($id);
-        if (!$area) {
-            return response()->json(['message' => 'Área não encontrada'], 404);
-        }
+        $msg = "Erro: usuários cadastrados com essa área";
 
-        if ($area->users()->exists()) {
-            return response()->json(['message' => 'Área vinculada a usuários'], 409);
+        $areas = Area::where('id', $id)->withCount(['usersInSubarea'])->get();
+        if(count($areas) > 0 and $areas[0]->users_in_subarea_count == 0){
+            $areas = Area::where('id', $id)->with('subarea')->get();
+            foreach($areas[0]->subarea as $subarea){
+                Subarea::deleteInstance($subarea->id);
+            }
+            return parent::destroy($id);
+        }elseif(count($areas) == 0){
+            return abort(406, "área não cadastrada");
+        }else{
+            return abort(406, $msg);
         }
-
-        $area->delete();
-        return response()->json(['message' => 'Área excluída com sucesso'], 200);
     }
 
     public function subareaPerArea(){
