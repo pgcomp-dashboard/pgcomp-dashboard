2cbf70a6b53ef42236919eba8e018bd8e85c1697
root <villa.junior@gmail.com>
2025-04-23 22:28:50 -0300

    Ajustes de dependencias

--- frontend-admin/vite.config.ts
+++ frontend-admin/vite.config.ts
@@ -3,12 +3,27 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [ react(), tailwindcss() ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      buffer: 'buffer',
+      util: 'util',
+    },
+  },
+  define: {
+    'process.env': {},
+    global: 'globalThis', 
+  },
+  optimizeDeps: {
+    include: ['crypto-browserify', 'stream-browserify', 'buffer', 'util'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['crypto', 'stream', 'buffer', 'util'],
     },
   },
 });
