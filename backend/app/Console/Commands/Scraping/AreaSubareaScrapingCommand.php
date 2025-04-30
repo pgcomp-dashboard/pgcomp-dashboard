b1a5b2460669be8618c462c36893c7afd6db2791
matheusjus <matheus.plima@trf1.jus.br>
2025-04-30 11:23:31 -0300

    chore(backend): Remove column "subarea" from table "areas"

--- backend/app/Console/Commands/Scraping/AreaSubareaScrapingCommand.php
+++ backend/app/Console/Commands/Scraping/AreaSubareaScrapingCommand.php
@@ -27,57 +27,18 @@ class AreaSubareaScrapingCommand extends Command
         // Get areas after the first h1 tag with the text "Área de Concentração"
         $areas = $dom->find('.region.region-content .field-items ul > li')->getIterator();
 
+        $this->getOutput()->info('Coletando dados...');
+        $areaNames = [];
         foreach ($areas as $area) {
             // Get the area name. Find until the first dot ".'
             $areaName = $area->text();
             $areaName = Str::of($areaName)->before('.')->trim()->title()->value();
 
-            $areaLink = $area->find('a')->attr('href');
-
-            $this->generateSubArea($areaName, $areaLink);
-        }
-    }
-
-    private function generateSubArea(string $areaName, string $areaUrl): void
-    {
-
-        $this->info("\nAcessando a área: $areaName");
-        $subAreas = $this->getSubAreas($areaName, $areaUrl);
-        $rows = [];
-        foreach ($subAreas as $subAreaName) {
-            $area = Area::updateOrCreate(
-                ['area' => $areaName, 'subarea' => $subAreaName]
+            Area::updateOrCreate(
+                ['area' => $areaName]
             );
-            $rows[] = [$area->id, $area->area, $area->subarea, $area->wasRecentlyCreated ? 'Sim' : 'Não'];
+            $areaNames[] = $areaName;
         }
-        $this->table(
-            ['ID', 'Área', 'Subárea', 'Criada'],
-            $rows
-        );
-    }
-
-    private function getSubAreas(string $areaName, string $areaUrl): array
-    {
-        // "Engenharia de Software" is a special case. PGCOMP site does not have an explicit subarea for it.
-        if ($areaName == "Engenharia De Software") {
-            return  [
-                "Engenharia de Software"
-            ];
-        }
-
-        // Access the area link and get the subareas
-        $client = new Client();
-        $response = $client->request('GET', $areaUrl);
-        $html = $response->getBody()->getContents();
-        $dom = html5qp($html);
-        $subAreasIterator = $dom->find('.region.region-content .field-items ul > li')->getIterator();
-
-        foreach ($subAreasIterator as $subArea) {
-            // Get the subarea name. Find until the first dot ".'
-            $subAreaName = $subArea->text();
-            $subAreaName = Str::of($subAreaName)->before('.')->trim()->title()->value();
-            $subAreas[] = $subAreaName;
-        }
-        return $subAreas;
+        $this->getOutput()->info('Áreas coletadas: ' . implode(', ', $areaNames));
     }
 }
