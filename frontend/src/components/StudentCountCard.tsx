c5282986541798906e8bbf38a8e307db732b21d7
Felipe Paixão <fleap@fleap.dev>
2025-06-02 19:00:00 -0300

    feat(frontend): convert student count chart into cards

---
+++ frontend/src/components/StudentCountCard.tsx
@@ -0,0 +1,29 @@
+import api from '@/services/api';
+import { useQuery } from '@tanstack/react-query';
+import {
+  NameType,
+  ValueType,
+} from 'recharts/types/component/DefaultTooltipContent';
+
+export default function StudentCountCard({
+  studentFilter,
+}: {
+  studentFilter: string;
+}) {
+  const { data, isLoading, error } = useQuery({
+    queryKey: [ 'number_of_students' ],
+    queryFn: () => api.numberOfStudents(),
+  });
+
+  if (isLoading) return <>Carregando...</>;
+  if (error) return <>Erro ao carregar o gráfico</>;
+
+  console.log(data);
+  const filteredData = data?.filter((d) => d.category == studentFilter);
+
+  return (
+    <div className="w-full h-full text-4xl flex-1 flex items-center justify-center">
+      {filteredData?.length ? filteredData[0].amount : 0}
+    </div>
+  );
+}
