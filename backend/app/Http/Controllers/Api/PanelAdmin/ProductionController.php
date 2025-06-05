e9db34cff69e82e6b736f0266ff7e902292e8007
rodrigoDev <rodriguinho2456@gmail.com>
2025-06-05 07:15:05 -0300

    feat: created  count journal and conference

--- backend/app/Http/Controllers/Api/PanelAdmin/ProductionController.php
+++ backend/app/Http/Controllers/Api/PanelAdmin/ProductionController.php
@@ -8,6 +8,7 @@ use App\Models\BaseModel;
 use App\Models\Production;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
 
 class ProductionController extends BaseApiResourceController
 {
@@ -54,5 +55,19 @@ class ProductionController extends BaseApiResourceController
             ->join('users', 'users_productions.users_id', '=', 'users.id')
             ->where('users.type', '=', UserType::PROFESSOR)
             ->where('users.id', '=', $professors);
+    }
+    public function getTypeCounts($professors)
+    {
+        return $this->newBaseQuery()
+            ->select([
+                DB::raw("SUM(CASE WHEN publisher_type = 'journal' THEN 1 ELSE 0 END) as journal_count"),
+                DB::raw("SUM(CASE WHEN publisher_type = 'conference' THEN 1 ELSE 0 END) as conference_count"),
+                DB::raw("COUNT(*) as total_count")
+            ])
+            ->join('users_productions', 'id', '=', 'users_productions.productions_id')
+            ->join('users', 'users_productions.users_id', '=', 'users.id')
+            ->where('users.type', '=', UserType::PROFESSOR)
+            ->where('users.id', '=', $professors)
+            ->first();
     }
 }
