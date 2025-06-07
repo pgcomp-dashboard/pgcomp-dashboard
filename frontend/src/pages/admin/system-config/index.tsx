2f90d6b7bf9a80300acecbc6b634c121677e3310
Felipe Paixão <fleap@fleap.dev>
2025-06-07 16:44:33 -0300

    feat(frontend): create mocked form to update scraping interval

--- frontend/src/pages/admin/system-config/index.tsx
+++ frontend/src/pages/admin/system-config/index.tsx
@@ -1,4 +1,35 @@
-export default function AreasPage() {
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+const systemConfigFormSchema = z.object({
+  scrapingIntervalDays: z.coerce.number({ message: 'Número inválido' }).min(1, 'Número precisa ser maior que 0'),
+});
+
+export default function SystemConfigPage() {
+  const form = useForm<z.infer<typeof systemConfigFormSchema>>({
+    resolver: zodResolver(systemConfigFormSchema),
+    defaultValues: {
+      scrapingIntervalDays: 7,
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof systemConfigFormSchema>) {
+    // TODO: remove this mock
+    console.log(values);
+  }
+
   return (
     <div className="space-y-6">
       <div className="flex items-center justify-between">
@@ -8,7 +39,28 @@ export default function AreasPage() {
         </div>
       </div>
       <div className="rounded-md border p-12">
-        <p>Formulário a definir...</p>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="scrapingIntervalDays"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Intervalo de scraping (dias)</FormLabel>
+                  <FormControl>
+                    <Input type='number' placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Intervalo em que será executado a tarefa de scraping para
+                    atualizar as informações na dashboard.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Atualizar</Button>
+          </form>
+        </Form>
       </div>
     </div>
   );
