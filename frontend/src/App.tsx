d0bbb5a26803b602f2abe8ed84854f2c33c8b172
cainanneves <cainan.lima@ufba.br>
2026-02-11 10:13:47 -0300

    feat: create base for publishers page, for now only show names

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,33 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter, Navigate, Outlet, Route, Routes } from 'react-router';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Navigate, Outlet, Route, Routes } from "react-router";
 
-import useAuth from '@/hooks/auth';
-import AdminLayout from '@/layouts/admin/admin-layout';
-import CredenciamentoPage from '@/pages/admin/accreditation';
-import AreasPage from '@/pages/admin/areas';
-import ProfessorsPage from '@/pages/admin/professors/professors-list';
-import QualisPage from '@/pages/admin/qualis/index';
-import StudentsPage from '@/pages/admin/students';
-import SystemConfigPage from '@/pages/admin/system-config';
-import ForgotPasswordPage from '@/pages/auth/forgot-password';
-import LoginPage from '@/pages/auth/login';
-import ResetPasswordPage from '@/pages/auth/reset-password';
-import NotFoundPage from '@/pages/not-found';
-import MyProductionsPage from '@/pages/user/my-productions';
-import UserConfigPage from '@/pages/user/user-config';
-import { AuthProvider } from '@/providers/AuthProvider';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Loader2 } from 'lucide-react';
-import { lazy, Suspense } from 'react';
-import { Toaster } from 'sonner';
-import { LoadingSpinner } from './components/LoadingSpinner';
-import AdminsPage from './pages/admin/admins';
-import ProfessorProductionsPage from './pages/admin/professors/production';
-import WelcomePage from './pages/user/welcome';
-
+import useAuth from "@/hooks/auth";
+import AdminLayout from "@/layouts/admin/admin-layout";
+import CredenciamentoPage from "@/pages/admin/accreditation";
+import AreasPage from "@/pages/admin/areas";
+import ProfessorsPage from "@/pages/admin/professors/professors-list";
+import QualisPage from "@/pages/admin/qualis/index";
+import StudentsPage from "@/pages/admin/students";
+import SystemConfigPage from "@/pages/admin/system-config";
+import ForgotPasswordPage from "@/pages/auth/forgot-password";
+import LoginPage from "@/pages/auth/login";
+import ResetPasswordPage from "@/pages/auth/reset-password";
+import NotFoundPage from "@/pages/not-found";
+import MyProductionsPage from "@/pages/user/my-productions";
+import UserConfigPage from "@/pages/user/user-config";
+import { AuthProvider } from "@/providers/AuthProvider";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Loader2 } from "lucide-react";
+import { lazy, Suspense } from "react";
+import { Toaster } from "sonner";
+import { LoadingSpinner } from "./components/LoadingSpinner";
+import AdminsPage from "./pages/admin/admins";
+import ProfessorProductionsPage from "./pages/admin/professors/production";
+import PublishersPage from "./pages/admin/publishers";
+import WelcomePage from "./pages/user/welcome";
 
 const queryClient = new QueryClient();
-const DashboardPage = lazy(() => import('./pages/admin/dashboard'));
+const DashboardPage = lazy(() => import("./pages/admin/dashboard"));
 
 function App() {
   return (
@@ -42,30 +42,43 @@ function App() {
 
             {/* Rotas Protegidas (Geral) */}
             <Route element={<EnsureAuthenticated />}>
-              <Route element={<AdminLayout><Outlet /></AdminLayout>}>
+              <Route
+                element={
+                  <AdminLayout>
+                    <Outlet />
+                  </AdminLayout>
+                }
+              >
                 <Route index path="/" element={<WelcomePage />} />
-                <Route path='portal' >
-                  <Route index element={<Navigate to='/' replace />} />
-                  <Route path='credenciamento' element={<CredenciamentoPage />} />
-                  <Route path='productions' element={<MyProductionsPage />} />
-                  <Route path='user-config' element={<UserConfigPage />} />
+                <Route path="portal">
+                  <Route index element={<Navigate to="/" replace />} />
+                  <Route
+                    path="credenciamento"
+                    element={<CredenciamentoPage />}
+                  />
+                  <Route path="productions" element={<MyProductionsPage />} />
+                  <Route path="user-config" element={<UserConfigPage />} />
                 </Route>
 
                 {/* Rotas restritas apenas para ADMIN */}
-                <Route path='admin' element={<EnsureAdmin />}>
-                  <Route index element={<Navigate to='/' replace />} />
-                  <Route path='team' element={<AdminsPage /> } />
-                  <Route path='areas' element={<AreasPage />} />
-                  <Route path='students' element={<StudentsPage />} />
-                  <Route path='professors' element={<ProfessorsPage />} />
-                  <Route path='professors/:professorId/productions' element={<ProfessorProductionsPage />} />
-                  <Route path='qualis' element={<QualisPage />} />
-                  <Route path='system-config' element={<SystemConfigPage />} />
+                <Route path="admin" element={<EnsureAdmin />}>
+                  <Route index element={<Navigate to="/" replace />} />
+                  <Route path="team" element={<AdminsPage />} />
+                  <Route path="areas" element={<AreasPage />} />
+                  <Route path="students" element={<StudentsPage />} />
+                  <Route path="professors" element={<ProfessorsPage />} />
+                  <Route
+                    path="professors/:professorId/productions"
+                    element={<ProfessorProductionsPage />}
+                  />
+                  <Route path="publishers" element={<PublishersPage />} />
+                  <Route path="qualis" element={<QualisPage />} />
+                  <Route path="system-config" element={<SystemConfigPage />} />
                 </Route>
               </Route>
-              <Route path='admin' element={<EnsureAdmin />}>
+              <Route path="admin" element={<EnsureAdmin />}>
                 <Route
-                  path='dashboard'
+                  path="dashboard"
                   element={
                     <Suspense fallback={<LoadingSpinner />}>
                       <DashboardPage />
@@ -73,8 +86,8 @@ function App() {
                   }
                 />
               </Route>
-             </Route>
-            <Route path='*' element={<NotFoundPage />} />
+            </Route>
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </BrowserRouter>
         <Toaster richColors position="top-right" />
@@ -94,8 +107,8 @@ function EnsureAuthenticated() {
   if (auth?.isAuthenticated) {
     return <Outlet />;
   } else {
-    console.error('User not authenticated. Redirecting back to login page...');
-    return <Navigate to='/login' replace />;
+    console.error("User not authenticated. Redirecting back to login page...");
+    return <Navigate to="/login" replace />;
   }
 }
 
@@ -104,10 +117,7 @@ function EnsureAdmin() {
 
   if (auth?.isLoading) return <Loader2 />;
 
-  return auth?.isAdmin
-    ? <Outlet />
-    : <Navigate to="/" replace />;
-
+  return auth?.isAdmin ? <Outlet /> : <Navigate to="/" replace />;
 }
 
 export default App;
---
+++ frontend/src/pages/admin/publishers/index.tsx
@@ -0,0 +1,71 @@
+import { Button } from "@/components/ui/button";
+import { publisherService } from "@/services/modules/publisher.service";
+import { Publisher } from "@/types/academic";
+import { PaginatedResponse } from "@/types/common";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+
+export default function PublishersPage() {
+  const [page, setPage] = useState(1);
+
+  const { data, isLoading, error, isPlaceholderData } = useQuery<
+    PaginatedResponse<Publisher>,
+    Error
+  >({
+    queryKey: ["publishers", page],
+    queryFn: async () => {
+      return await publisherService.getAllPublishers(page);
+    },
+    placeholderData: keepPreviousData,
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error fetching data</div>;
+
+  return (
+    <>
+      {data?.data?.map((publisher) => (
+        <div>{publisher.name}</div>
+      ))}
+       <div className="flex items-center justify-between p-4">
+          <span className="text-sm text-muted-foreground">
+            Página {page} de {data?.meta.last_page}
+          </span>
+          <div className="flex gap-2 items-center">
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => setPage(1)}
+              disabled={page=== 1}
+            >
+              {'<<'}
+            </Button>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
+              disabled={page=== 1}
+            >
+              ‹ Anterior
+            </Button>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => setPage((prev) => Math.min(prev + 1, data?.meta.last_page || 1))}
+              disabled={page=== data?.meta.last_page}
+            >
+              Próxima ›
+            </Button>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => setPage(data?.meta.last_page || 1)}
+              disabled={page=== data?.meta.last_page}
+            >
+              {'>>'}
+            </Button>
+          </div>
+        </div>
+    </>
+  );
+}
