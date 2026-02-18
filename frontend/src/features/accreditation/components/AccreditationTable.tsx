658e33a8b9a69f153de540e9c873f95e6c923c8c
cainanneves <cainan.lima@ufba.br>
2026-02-18 20:11:46 -0300

    fix: accreditation page usememo uses function

--- frontend/src/features/accreditation/components/AccreditationTable.tsx
+++ frontend/src/features/accreditation/components/AccreditationTable.tsx
@@ -89,7 +89,7 @@ export function AccreditationTable({
         ),
       }),
     ],
-    [],
+    [onShowDetails],
   );
 
   return (
--- frontend/src/features/areas/components/AreaTable.tsx
+++ frontend/src/features/areas/components/AreaTable.tsx
@@ -60,7 +60,7 @@ export function AreaTable({
         ),
       }),
     ],
-    [onEdit, onDelete]
+    [onEdit, onDelete],
   );
 
   return (
